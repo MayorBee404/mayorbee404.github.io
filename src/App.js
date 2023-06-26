525904c6802e5b9e8a699ec3b3b8c1d063efa8fc
Daninsyah Bagas Abiyansa <63465768+MayorBee404@users.noreply.github.com>
2023-06-26 09:57:57 +0700

    implamant about me

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     email: "abiyansa.jobs@aol.com",
     availability: "Open for work",
     brand:
-      "I possess a strong foundation in Android development and a passion for mobile app technologies. I am currently focused on expanding my expertise in software development and have hands-on experience building apps using Agile Scrum, Kanban, and Extreme Programming methodologies.",
+      "I possess a strong foundation in Android development and a passion for mobile app technologies. I am currently focusing on expanding my expertise in software development and have hands-on experience building apps using Agile Scrum, Kanban, and Extreme Programming methodologies.",
   }
 
   return (
