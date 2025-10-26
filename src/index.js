a153a40c223a8bc51b57edeee6903f6f3c44b963
Daninsyah Bagas Abiyansa <63465768+MayorBee404@users.noreply.github.com>
2025-10-26 12:01:17 +0700

    Fix basename in BrowserRouter for correct routing

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/portofolio-abiyansa">
+    <BrowserRouter basename="/portofolio-abiyansa/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
